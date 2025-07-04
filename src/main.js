e5fa4f090383d5453aa87a508efa08dd7cfd878f
locitakaes42 <locitakaes@gmail.com>
2025-07-04 10:25:34 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import 'admin-lte/dist/css/adminlte.min.css'
 import 'admin-lte/plugins/fontawesome-free/css/all.min.css'
 
 import 'admin-lte';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'admin-lte/plugins/jquery/jquery.min.js';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+//import 'admin-lte/plugins/jquery/jquery.min.js';
 
 const app = createApp(App)
 app.use(router)
