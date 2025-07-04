98a94eafb594628a6aeec9d8614f25c5dcce0b10
locitakaes42 <locitakaes@gmail.com>
2025-07-04 09:42:46 +0700

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
+// locitakaes42/km-dashboard-covid/KM-Dashboard-Covid-9a335753b8924185e55f86bda86eb3ff78072743/vite.config.js
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,6 +12,18 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  optimizeDeps: {
+    include: ['bootstrap'], // Ini membantu Vite memahami bootstrap di awal (saat dev)
+                            // terkadang membantu di build juga
+  },
+  build: {
+    rollupOptions: {
+      // Tambahkan konfigurasi eksternal jika Anda TIDAK ingin Bootstrap dibundel ke dalam JS Anda
+      // dan Anda akan menyediakannya secara global melalui tag <script> di index.html.
+      // Jika Anda ingin Bootstrap dibundel, JANGAN tambahkan ini.
+      // external: ['bootstrap/dist/js/bootstrap.bundle.min.js'],
     }
   }
 })
